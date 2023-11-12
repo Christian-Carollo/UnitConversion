ba9289eada075bc2d299262eeb8b83c056ea5f26
Administrator <Administrator@DESKTOP-95RB6IG>
2023-11-12 12:20:21 +0100

    Aggiungere i file di progetto.

---
+++ UnitConversion/Program.cs
@@ -0,0 +1,23 @@
+﻿Console.WriteLine("Inserisci la temperatura in gradi Celsius:");
+
+string inputUtente = Console.ReadLine();
+
+if (double.TryParse(inputUtente, out double celsius))
+{
+    double fahrenheit = ConvertCelsiusToFahrenheit(celsius);
+
+    Console.WriteLine($"{celsius} gradi Celsius corrispondono a {fahrenheit} gradi Fahrenheit.");
+}
+else
+{
+    Console.WriteLine("Inserimento non valido. Assicurati di inserire un numero.");
+}
+
+
+
+
+
+double ConvertCelsiusToFahrenheit(double celsius)
+{
+    return (celsius * 32) + 18;
+}
